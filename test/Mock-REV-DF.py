ce0906fb2c9e9695c24d0474e5a817cefce4bb05
Parker Erway <parker@erwaysoftware.com>
2021-10-21 11:28:17 -0600

    Sync mock scripts to updated Unipolar scripts

--- test/Mock-REV-DF.py
+++ test/Mock-REV-DF.py
@@ -1,41 +1,47 @@
 #!/usr/bin/env python3
+########################################################################
+# Filename    : UnipolarRev.py
+# Author      : danelu
+# modification: 2018/08/02
 # ReverseOnly
+########################################################################
 import time
 import sys
 
-t1 = 0.003  # power time for energizing the coils
-# t = input ("Speed?/msecs 0=max; 50=min " )
-# tp=int(t)
-t2 = 0.003  # pause time between pulses
+def do_steps_rev(args):
+
+    t1 = 0.003  # power time for energizing the coils
+    # t = input ("Speed?/msecs 0=max; 50=min " )
+    # tp=int(t)
+    t2 = 0.003  # pause time between pulses
 
 
-def setStep(w1, w2, w3, w4):
-    time.sleep(0.0025)
-    pass
+    def setStep(w1, w2, w3, w4):
+        time.sleep(0.0025)
 
-if len(sys.argv) == 1:
-    n = input("Steps? ")  # how many steps to execute
-else:
-    n = sys.argv[1]
-m = int(n)
+    if args == "":
+        args = input("Steps? ")  # how many steps to execute
+    m = int(args)
+    for i in range(0, m):
+        setStep(1, 0, 0, 0)
+        time.sleep(t1)
+        setStep(0, 0, 0, 0)
+        time.sleep(t2)
 
-for i in range(0, m):
-    setStep(1, 0, 0, 0)
-    time.sleep(t1)
-    setStep(0, 0, 0, 0)
-    time.sleep(t2)
+        setStep(0, 1, 0, 0)
+        time.sleep(t1)
+        setStep(0, 0, 0, 0)
+        time.sleep(t2)
 
-    setStep(0, 1, 0, 0)
-    time.sleep(t1)
-    setStep(0, 0, 0, 0)
-    time.sleep(t2)
+        setStep(0, 0, 1, 0)
+        time.sleep(t1)
+        setStep(0, 0, 0, 0)
+        time.sleep(t2)
 
-    setStep(0, 0, 1, 0)
-    time.sleep(t1)
-    setStep(0, 0, 0, 0)
-    time.sleep(t2)
+        setStep(0, 0, 0, 1)
+        time.sleep(t1)
+        setStep(0, 0, 0, 0)
+        time.sleep(t2)
 
-    setStep(0, 0, 0, 1)
-    time.sleep(t1)
-    setStep(0, 0, 0, 0)
-    time.sleep(t2)
+if __name__ == '__main__':
+    do_steps_rev("") if len(sys.argv) == 1 else do_steps_rev(sys.argv[1])
