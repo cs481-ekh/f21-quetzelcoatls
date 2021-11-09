f745c4c6cb849d40a942f61ea0a630acb7ad3455
amickel <adriannamickel@u.boisestate.edu>
2021-11-09 14:54:25 -0700

    text box field for micrometer with input validation

--- RPfiles/frontend.py
+++ RPfiles/frontend.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import PySimpleGUI as sg
+import re
 
 forward_script = "mock/Mock-FWD-DF.py"
 reverse_script = "mock/Mock-REV-DF.py"
@@ -7,6 +8,7 @@ reverse_script = "mock/Mock-REV-DF.py"
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
 layout = [  [sg.Button("Stop", key="Stop", visible=False), sg.Text(key="Status")],
+            [sg.Text('Micrometer input:'), sg.InputText(key="micro",enable_events = True, do_not_clear=True)],
             [sg.Text('Steps forward'), sg.InputText(), sg.Button('Fwd', key="Fwd")],
             [sg.Text('Steps reverse'), sg.InputText(), sg.Button('Rev', key="Rev")],
             [sg.Button('Return to 0', key="Ret0")],
@@ -85,6 +87,10 @@ while 1:
                 stepsTracker = 0
             else:
                 print("Stepper is already at 0")
+        elif event == "micro":
+            if re.fullmatch("\d.\d{2,3}",values["micro"]) == None:
+                window["micro"].update("")
+
     except ValueError:
         print("Invalid step count specified")
 
